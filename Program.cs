610a6fe9af216b406b43893cdd9b266d732234a6
Hossein Maani <mh.maani279@outlook.com>
2023-06-12 17:22:53 +0330

    some check

--- Program.cs
+++ Program.cs
@@ -1,44 +1,59 @@
 ﻿string baseResult = "";
 Console.Write("Enter Number:");
-int number = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine()!);
 
 Console.Write("Enter You Base:");
-int toBase = int.Parse(Console.ReadLine());
+int toBase = int.Parse(Console.ReadLine()!);
 
-
-
-while (number != 0)
+if (!CheckValidBase(toBase))
 {
-    int remain = number % toBase;
-    if (remain < 10)
-        baseResult += remain;
-    else
+    Console.WriteLine("Invalid Base");
+}
+else
+{
+    while (number != 0)
     {
-        if (remain == 10)
+        int remain = number % toBase;
+        if (remain < 10)
+            baseResult += remain;
+        else
         {
-            baseResult += "A";
+            if (remain == 10)
+            {
+                baseResult += "A";
+            }
+            else if (remain == 11)
+            {
+                baseResult += "B";
+            }
+            else if (remain == 12)
+            {
+                baseResult += "C";
+            }
+            else if (remain == 13)
+            {
+                baseResult += "D";
+            }
+            else if (remain == 14)
+            {
+                baseResult += "E";
+            }
+            else if (remain == 15)
+            {
+                baseResult += "F";
+            }
         }
-        else if(remain == 11){
-            baseResult += "B";
-        }
-        else if(remain == 12){
-            baseResult += "C";
-        }
-        else if(remain == 13){
-            baseResult += "D";
-        }
-        else if(remain == 14){
-            baseResult += "E";
-        }
-        else if(remain == 15){
-            baseResult += "F";
-        }
+        number /= toBase;
     }
-    number /= toBase;
-}
 
-for (int i = baseResult.Length - 1; i >= 0; i--)
+    for (int i = baseResult.Length - 1; i >= 0; i--)
+    {
+        Console.Write(baseResult[i]);
+    }
+}
+bool CheckValidBase(int toBase)
 {
-    Console.Write(baseResult[i]);
+    if (toBase == 2 || toBase == 8 || toBase == 10 || toBase == 16)
+        return true;
+    return false;
 }
-

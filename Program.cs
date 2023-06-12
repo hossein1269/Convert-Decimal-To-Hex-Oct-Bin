976307d645afd99aa23fa4204c81a8db4666c859
Hossein Maani <mh.maani279@outlook.com>
2023-06-12 17:16:13 +0330

    init

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿string baseResult = "";
+Console.Write("Enter Number:");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write("Enter You Base:");
+int toBase = int.Parse(Console.ReadLine());
+
+
+
+while (number != 0)
+{
+    int remain = number % toBase;
+    if (remain < 10)
+        baseResult += remain;
+    else
+    {
+        if (remain == 10)
+        {
+            baseResult += "A";
+        }
+        else if(remain == 11){
+            baseResult += "B";
+        }
+        else if(remain == 12){
+            baseResult += "C";
+        }
+        else if(remain == 13){
+            baseResult += "D";
+        }
+        else if(remain == 14){
+            baseResult += "E";
+        }
+        else if(remain == 15){
+            baseResult += "F";
+        }
+    }
+    number /= toBase;
+}
+
+for (int i = baseResult.Length - 1; i >= 0; i--)
+{
+    Console.Write(baseResult[i]);
+}
+
